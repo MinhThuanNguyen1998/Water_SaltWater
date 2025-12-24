9f0a40cf4c37200db1747d8ab1ee9af2e77982ad
thuannm <thuannm.ot@gmail.com>
2025-12-24 11:15:37 +0700

    + Add : function change color bunsen

---
+++ Assets/Master/Scripts/Object/ObjectColorController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectColorController : MonoBehaviour
+{
+    [SerializeField] private bool m_GetStartingColorFromMaterial;
+    [SerializeField] private FlexibleColorPicker m_Fcp;
+    [SerializeField] private Material m_NewMaterial;
+    [SerializeField] private Material m_DefaultMaterial;
+    [SerializeField] private Button m_ResetDefaultMaterialButton;
+
+    private void Start()
+    {
+        if (m_GetStartingColorFromMaterial)
+            m_Fcp.color = m_NewMaterial.color;
+        m_Fcp.onColorChange.AddListener(OnChangeColor);
+        m_ResetDefaultMaterialButton.onClick.AddListener(OnButtonResetDefaultMaterial);
+    }
+    private void OnChangeColor(Color co)
+    {
+        m_NewMaterial.color = co;
+    }
+    private void OnButtonResetDefaultMaterial()
+    {
+        m_Fcp.color = m_DefaultMaterial.color;
+    }
+}
