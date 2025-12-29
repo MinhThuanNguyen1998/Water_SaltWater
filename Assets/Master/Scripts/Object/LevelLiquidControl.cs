382ccb6e637f12bdb3d2eef265c881bb44c2120d
thuannm <thuannm.ot@gmail.com>
2025-12-29 10:23:38 +0700

    - Update : lighter trigger, vase trigger - Update : new vase

--- Assets/Master/Scripts/Object/LevelLiquidControl.cs
+++ Assets/Master/Scripts/Object/LevelLiquidControl.cs
@@ -5,7 +5,9 @@ public class LevelLiquidControl : MonoBehaviour
 {
     [SerializeField] private Renderer m_Renderer;
     private Material m_Material;
-    private float m_FillValue = 0f;
+    private float m_DefaultFill = -0.3f;
+    private float m_MaxFill = 0f;
+    private float m_FillValue;
     public float CurrentFill
     {
         get => m_FillValue;
@@ -20,10 +22,17 @@ public class LevelLiquidControl : MonoBehaviour
         if (m_Renderer != null) m_Material = m_Renderer.sharedMaterial;
         ResetFillLevel();
     }
+    public void AddFill(float delta)
+    {
+        SetFill(m_FillValue + delta);
+    }
+    public void SetFill(float value)
+    {
+        m_FillValue = Mathf.Clamp(value, m_DefaultFill, m_MaxFill);
+        UpdateFillLevel();
+    }
+    public void ResetFillLevel() => SetFill(m_DefaultFill);
 
-    public void ControlFillLevel(float value) => CurrentFill = value;
-    public void ResetFillLevel() => ControlFillLevel(0f);
-    
     private void UpdateFillLevel() => m_Material?.SetFloat("_Fill", m_FillValue);
  
 }
--- Assets/Master/Scripts/Object/ObjectTriggers/VaseDestroyerDropletTrigger.cs
+++ Assets/Master/Scripts/Object/ObjectTriggers/VaseDestroyerDropletTrigger.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 public class VaseDestroyerDropletTrigger : BaseTrigger
 {
     [SerializeField] private LevelLiquidControl m_LevelLiquidVaseControl;
-    private float m_FillValue = 0.01f;
-    private float m_MaxFillValue = 0.2f;
+    private float m_FillValue = 0.02f;
+   
     protected override void OnEnter(Collider other)
     {
         if (other.gameObject.tag == "Droplet")
         {
-            float newFill = Mathf.Clamp(m_LevelLiquidVaseControl.CurrentFill + m_FillValue, 0f, m_MaxFillValue);
-            m_LevelLiquidVaseControl.ControlFillLevel(newFill);
+            m_LevelLiquidVaseControl.AddFill(m_FillValue);
             AudioMainManager.Instance.PlayOnShot(SoundType.WaterDrip);
             Destroy(other.gameObject);
         }
